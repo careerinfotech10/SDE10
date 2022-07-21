1cae3ba25873ba9dcfd740690d829a18043d3b64
careerinfotech10 <ssturerao@gmail.com>
2022-07-21 20:27:42 +0530

    updated pom

--- microservices/studentmanagement/src/main/java/com/career/revenue/service/IncomeService.java
+++ microservices/studentmanagement/src/main/java/com/career/revenue/service/IncomeService.java
@@ -1,5 +1,21 @@
 package com.career.revenue.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.career.revenue.entity.Income;
+import com.career.revenue.repo.IncomeRepo;
+
+@Service
 public class IncomeService {
+
+	@Autowired
+	private IncomeRepo incomeRepo;
+	public List<Income> getIncomes() {
+		// TODO Auto-generated method stub
+		return incomeRepo.findAll();
+	}
 
 }
